a3a320c31c0a8198b3f3df1879fbfdd984802587
lauraagalvan <126706348+lauraagalvan@users.noreply.github.com>
2023-03-03 13:47:09 +0100

    Add files via upload

---
+++ ejercicio 3.1/src/Main.java
@@ -0,0 +1,73 @@
+public class Main {
+
+    public static void main(String[] args) {
+        var estacion = "verano";
+
+        switch(estacion) {
+            case "verano":
+                System.out.println("es verano");
+                break;
+            case "invierno":
+                System.out.println("es invierno");
+                break;
+            case "primavera":
+                System.out.println("es primavera");
+                break;
+            case "otoño":
+                System.out.println("es otoño");
+                break;
+            default:
+                System.out.println("no es la estacion");
+        }
+
+        public static void main(String[] args) {
+            int contador = 3;
+
+
+            while (contador > 0){
+                System.out.println(contador);
+                contador = contador -1;
+
+
+
+
+            }
+            public static void main(String[] args) {
+                int contador = 3;
+
+
+                do {
+                    System.out.println(contador);
+                    contador = contador -1;
+                } while (contador > 10);
+            }
+            public static void main(String[] args) {
+                for (int contador = 0;contador <= 3; contador = contador +1 ) {
+                    System.out.println(contador);
+
+
+                }
+
+            public static void main(String[] args) {
+        var estacion = "verano";
+
+        switch(estacion) {
+            case "verano":
+                System.out.println("es verano");
+                break;
+            case "invierno":
+                System.out.println("es invierno");
+                break;
+            case "primavera":
+                System.out.println("es primavera");
+                break;
+            case "otoño":
+                System.out.println("es otoño");
+                break;
+            default:
+                System.out.println("no es la estacion");
+        }
+    }
+
+
+}
