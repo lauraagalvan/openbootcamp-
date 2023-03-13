66c86397eb4908c4982c29f16fce0c7b2ae5fe5b
lauraagalvan <126706348+lauraagalvan@users.noreply.github.com>
2023-03-13 13:10:51 +0100

    Add files via upload

---
+++ EjercicioJava1/src/EjercicioJava/JavaMain.java
@@ -0,0 +1,23 @@
+package EjercicioJava;
+
+public class JavaMain {
+
+    public static void main(String[] args) {
+        byte numero1 = 3;
+        short numero2 = 15;
+        int numero3 = 30;
+        long numero4 = 1000;
+
+        float numero5 = 5.15f;
+        double numero6 = 10.80d;
+
+        boolean variable1 = false;
+        boolean variable2 = true;
+
+
+        char variable3 = "a";
+        String variable4 = "Hola mundo";
+
+        System.out.println();
+    }
+}
