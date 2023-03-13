0fbc2e08b92a2ec89e5437efe59cbedaf806e186
lauraagalvan <126706348+lauraagalvan@users.noreply.github.com>
2023-03-13 15:22:55 +0100

    Add files via upload

---
+++ EjercicioJava2/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int precio = 10;
+        double iva = 2.10;
+
+        System.out.println(precio + iva);
+
+
+    }
+
+}
